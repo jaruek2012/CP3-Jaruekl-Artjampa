ecb24ccc154c3e963ca31c2441ad85861f0d2d36
jaruek2012 <151112603+jaruek2012@users.noreply.github.com>
2023-12-07 10:49:47 +0700

    Create Exercise4_Jaruek_A.py

---
+++ Exercise4_Jaruek_A.py
@@ -0,0 +1,9 @@
+SEng = 98.5
+SBus = 85
+SComs = 88
+SComp = 96.8
+print("---Your Score---")
+print("Foundation English :","",SEng)
+print("General Business :","",SBus)
+print("Introduction to Computer Systems :","",SComs)
+print("Computer Programming :","",SComp)
